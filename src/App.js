ba6bda01baf3236e5c6ba61ec52b0e30586eae7f
PlomerFranco <franko.plomer@gmail.com>
2021-07-22 14:13:50 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import React, {useState,useEffect} from 'react';
 import './App.css';
+const ApiURL = 'https://api.giphy.com/v1/gifs/search?api_key=ukzi7JHMHcx7AAjHJffLmBkQN5amm1M7&q=panda&limit=25&offset=0&rating=g&lang=en';
+
 
 function App() {
+  const [gifs, setGifs] = useState([])
+
+  useEffect(function () 
+  {
+    fetch(ApiURL)
+        .then(res => res.json())
+        .then(response =>   
+          {
+            const {data} = response
+            const gifs = data.map(image => image.images.downsized_medium.url)
+            setGifs(gifs)
+          })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gifs.map(singleGif => <img src={singleGif}/>)
+        }
+      </section>
     </div>
   );
 }
